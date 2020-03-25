3cb723c0a653153c2e9f735ee1c9a7c83f62f5c3
Gonzalo <gonzalom97@outlook.com>
2020-03-25 14:02:35 -0300

    style: Refactorizacion de tabla.js
    - Se agregaron TODOs

--- Practica_UI/src/js/table.js
+++ Practica_UI/src/js/table.js
@@ -1,28 +1,32 @@
 ﻿$(document).ready(function () {
-	var tableHtml = $("#tablePersons");
+	let tableHtml = $("#tablePersons");
 
-	$.ajax({
-		url: "https://localhost:5001/api/Table/GetPersons",
-		method: "GET",
-		dataType: 'json',
-		success: function (data) {
-			let persons = data;
+	const cargarDatosATabla = () => {
+		$.ajax({
+			url: "https://localhost:5001/api/Table/GetPersons",
+			method: "GET",
+			dataType: 'json',
+			success: function (data) {
+				let persons = data;
 
-			let html = insertarTabla(persons);
+				let html = insertarTabla(persons);
 
-			tableHtml.append(html);
+				tableHtml.append(html);
+			},
+			error: (error) => console.log(JSON.stringify(error))
+		});
+	};
 
-			//else if (res.Status == "300") {
-			//	crearUsuario(userName, password, "Grilla2.html")
-			//}
-
-		},
-		error: (error) => console.log(JSON.stringify(error))
-	});
-
-
-	var insertarTabla = (persons) => {
+	const insertarTabla = (persons) => {
 		let html = "";
+
+		const getFilaCon = (fullName, dni) => {
+			return `
+				<tr>
+					<td> ${fullName} </td>
+					<td> ${dni}  </td>
+				</tr>`
+		};
 
 		persons.map((p) => {
 			html += getFilaCon(p.fullName, p.dni);
@@ -31,10 +35,8 @@
 		return html;
 	};
 
-	var getFilaCon = (fullName, dni) => {
-		return `<tr>
-					<td> ${fullName} </td>
-					<td> ${dni}  </td>
-				</tr>`
-	};
+	cargarDatosATabla();
+
+	//TODO: Peticion para insertar Personas a la tabla
+	
 })
