5ae7c71e17c1ce8caa68acd3c710062bb1ed5a45
Gonzalo <gonzalom97@outlook.com>
2020-04-02 10:36:50 -0300

    fix: Ocultacion de modal al hacer click en el boton principal del mismo

--- Practica_UI/src/js/login.js
+++ Practica_UI/src/js/login.js
@@ -1,48 +1,42 @@
 ﻿$(document).ready(function () {
 
     const ajaxlogin = (userName, password) => {
-        if (userName != "" && password.length >= 5) {
-            let data = { userName, password };
+        let data = { userName, password };
 
-            $.ajax({
-                url: "https://localhost:5001/api/LoginCrud/Login",
-                method: "POST",
-                data: JSON.stringify(data),
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: ({ message }) => {
-                    window.location = "https://localhost:44326/" + message;
-                },
-                error: (error) => console.log(error.responseText)
-            });
-        }
-        else alert("Complete los campos")
+        return $.ajax({
+            url: "https://localhost:5001/api/LoginCrud/Login",
+            method: "POST",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: ({ message }) => {
+                window.location = "https://localhost:44326/" + message;
+            },
+            error: (error) => console.log(error.responseText)
+        });
     }
 
     const ajaxChangePassword = (userName, password, newPassword) => {
-        if (userName != "" && newPassword.length >= 5) {
-            let user = { userName, password };	 
-            let data = { user, newPassword };
+        let user = { userName, password };	 
+        let data = { user, newPassword };
 
-            $.ajax({
-                url: "https://localhost:5001/api/LoginCrud/UpdatePassword",
-                method: "PUT",
-                data: JSON.stringify(data),
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: ({ message }) => {
-                    alert(message);
-                },
-                error: (error) => alert(error.responseText)
-            });
-        }
-        else alert("Complete los campos");
+        return $.ajax({
+            url: "https://localhost:5001/api/LoginCrud/UpdatePassword",
+            method: "PUT",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: ({ message }) => {
+                alert(message);
+            },
+            error: (error) => alert(error.responseText)
+        });
     }
 
     const ajaxCreateUser = (userName, password, defaultPage) => {
         let data = { userName, password, defaultPage };
 
-        $.ajax({
+        return $.ajax({
             url: "https://localhost:5001/api/LoginCrud/CreateUser",
             method: "POST",
             data: JSON.stringify(data),
@@ -59,16 +53,25 @@
         let userName = $("#fEmail").val();
         let password = $("#fPass").val();
 
-        ajaxlogin(userName, password);
-
+        if (userName != "" && password.length >= 5)
+            ajaxlogin(userName, password);
+        else
+            alert("Complete los campos")
     })
 
     $("#btnAceptChangePass").click(function (e) {
-        let userName = $("#fNewPassEmail").val();
-        let oldPass = $("#fOldPass").val();
-        let newPass = $("#fNewPass").val();
+        let userName = $("#fNewPassEmail").val().trim();
+        let oldPass = $("#fOldPass").val().trim();
+        let newPass = $("#fNewPass").val().trim();
 
-        ajaxChangePassword(userName, oldPass, newPass)
+        if (userName != "" && oldPass.length >= 5 && newPass.length >= 5) {
+            ajaxChangePassword(userName, oldPass, newPass)
+                .then(res => {
+                    $("#changePassModal").modal('hide');
+                });
+        }
+        else
+            alert("Complete los campos");
     });
 
     $("#btnCreateNewUser").click(function (e) {
@@ -76,8 +79,10 @@
         let password = $("#fNewUserPass").val();
         let defaultPage = $("#fNewUserPage").val();
 
-        ajaxCreateUser(userName, password, defaultPage);
-
+        ajaxCreateUser(userName, password, defaultPage)
+            .then(res => {
+                $("#newUserModal").modal('hide');
+            });
     });
 
 });
