7e1161a69042c1cb02ffb95029d2928c4ec61469
Gonzalo <gonzalom97@outlook.com>
2020-04-07 11:50:39 -0300

    feat: Redireccionamiento a Login al expirar token
    Hubo modificaciones para aceptar dni con 7 digitos

--- Practica_UI/src/js/table.js
+++ Practica_UI/src/js/table.js
@@ -1,93 +1,98 @@
 ﻿$(document).ready(function () {
-	let tableHtml = $("#tablePersons");
-	let miAlert = $("#miAlert");
-	let token = localStorage.getItem('token');
+    let tableHtml = $("#tablePersons");
+    let miAlert = $("#miAlert");
+    let token = localStorage.getItem('token');
 
-	const ajaxLoadTableData = (token) => {
-		debugger
-		$.ajax({
-			url: `https://localhost:5003/api/Table/GetPersons?token=${token}`,
-			method: "GET",
-			dataType: 'json',
-			success: function (data) {
-				let persons = data;
-				let html = generateTableOf(persons);
+    const ajaxLoadTableData = (token) => {
+        return $.ajax({
+            url: `https://localhost:5003/api/Table/GetPersons?token=${token}`,
+            method: "GET",
+            dataType: 'json',
+            success: function (data) {
+                let persons = data;
+                let html = generateTableOf(persons);
 
-				tableHtml.append(html);
-			},
-			error: (error) => alertError(error)
-		});
-	};
+                tableHtml.append(html);
+            }
+        });
+    };
 
-	const ajaxAddPerson = (name, surName, dni) => {
-		let data = { name, surName, dni };
+    const ajaxAddPerson = (name, surName, dni) => {
+        let data = { name, surName, dni };
 
-		$.ajax({
-			url: "https://localhost:5003/api/Table/AddPerson",
-			type: "PUT",
-			data: JSON.stringify(data),
-			dataType: "json",
-			contentType: "application/json; charset=utf-8",
-			success: (personAdded) => {
-				tableHtml.append(generateTableOf([personAdded]));
-				showAlert("Se agrego una persona.", "success");
-			},
-			error: (error) => alertError(error)
-		});
-	}
+        $.ajax({
+            url: "https://localhost:5003/api/Table/AddPerson",
+            type: "PUT",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: (personAdded) => {
+                tableHtml.append(generateTableOf([personAdded]));
+                showAlert("Se agrego una persona.", "success");
+            },
+            error: (error) => alertError(error)
+        });
+    }
 
-	const ajaxDeletePerson = (dniPerson) => {
-		let parameters = `?dniPerson=${dniPerson}`
-		return $.ajax({
-			url: `https://localhost:5003/api/Table/DeletePerson${parameters}`,
-			type: "DELETE",
-			success: (data) => {
-				showAlert(data.message, "success");
-			},
-			error: (error) => alertError(error)
-		});
-	}
+    const ajaxDeletePerson = (dniPerson) => {
+        let parameters = `?dniPerson=${dniPerson}`
+        return $.ajax({
+            url: `https://localhost:5003/api/Table/DeletePerson${parameters}`,
+            type: "DELETE",
+            success: (data) => {
+                showAlert(data.message, "success");
+            },
+            error: (error) => alertError(error)
+        });
+    }
 
-	ajaxLoadTableData(token);
+    ajaxLoadTableData(token)
+        .catch(error => {
+            alertError(error, "Se te redireccionara en unos segundos");
+            if (error.status === 401) {
+                setTimeout(function () {
+                    window.location.href = window.origin += "/Login.html";
+                }, 5000);
+            }
+        });
 
 
 
-	$("#btnAddPerson").click(function () {
-		let name = $("#fName").val().trim();
-		let surName = $("#fSurName").val().trim();
-		let dni = parseInt($("#fDni").val().trim());
+    $("#btnAddPerson").click(function () {
+        let name = $("#fName").val().trim();
+        let surName = $("#fSurName").val().trim();
+        let dni = parseInt($("#fDni").val().trim());
+        let cantDigitosDni = Math.floor(Math.log10(dni)) + 1;
 
-		let cantDigitosDni = Math.floor(Math.log10(dni)) + 1;
-
-		if (name !== "" && surName !== "" && cantDigitosDni >= 8)
-			ajaxAddPerson(name, surName, dni);
-		else
-			showAlert("Ingrese los datos para continuar o verifíquelos", "info");
-	});
+        if (name !== "" && surName !== "" && cantDigitosDni >= 7)
+            ajaxAddPerson(name, surName, dni);
+        else
+            showAlert("Ingrese los datos para continuar o verifíquelos", "info");
+    });
 
 	/*
 	 * Delegacion de eventos: En vez de escuchar a tdodos los botones 
 	 * se lo hace a su contenedor principal que es la tabla.
 	*/
-	$("#tablePersons").on("click", ".btnDeletePerson", function () {
-		//Se definio en el ID de cada celda el dni de cada persona
-		let dniPersonToDelete = $(this).parent().parent().attr("id");
+    $("#tablePersons").on("click", ".btnDeletePerson", function () {
+        //Se definio en el ID de cada celda el dni de cada persona
+        let dniPersonToDelete = $(this).parent().parent().attr("id");
 
-		ajaxDeletePerson(dniPersonToDelete)
-			.then(res => {
-				let rowToDelete = $(this).parent().parent();
-				$(rowToDelete).remove();
-			})
-	});
+        ajaxDeletePerson(dniPersonToDelete)
+            .then(res => {
+                let rowToDelete = $(this).parent().parent();
+                $(rowToDelete).remove();
+            })
+    });
 
 
-	// Funcionalidades
-	const generateTableOf = (persons) => {
-		//persons: Array de personas
-		let html = "";
+    // Funcionalidades
+    const generateTableOf = (persons) => {
+        //persons: Array de personas
+        let html = "";
 
-		const createFilaWhit = (name, surName, dni) => {
-			return `
+        const createFilaWhit = (name, surName, dni) => {
+            return `
 				<tr id="${dni}">
 					<td>${name}</td>
 					<td>${surName}</td>
@@ -98,17 +103,17 @@
 						</button>
 					</td>
 				</tr>`
-		};
+        };
 
-		persons.map((p) => {
-			html += createFilaWhit(p.name, p.surName, p.dni);
-		});
+        persons.map((p) => {
+            html += createFilaWhit(p.name, p.surName, p.dni);
+        });
 
-		return html;
-	};
+        return html;
+    };
 
-	const showAlert = (msg, type) => {
-		let alertHtml = `
+    const showAlert = (msg, type) => {
+        let alertHtml = `
 		<div id="innerAlert" class="alert alert-${type} alert-dismissible fade" role="alert">
 			<p> ${msg}</p>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -116,37 +121,37 @@
             </button>
         </div>
 		`
-		miAlert.html(alertHtml);
+        miAlert.html(alertHtml);
 
-		$("#innerAlert").toggleClass("show");
-	};
+        $("#innerAlert").toggleClass("show");
+    };
 
-	const alertError = (error) => {
-		let typeError = "danger";
+    const alertError = (error, secundaryMsg) => {
+        let typeError = "danger";
 
-		if (error.readyState !== 0) {
-			let status = `Error ${error.status}:`;
-			if (error.responseJSON !== undefined)
-				showAlert(`${status} ${JSON.stringify(error.responseJSON.errors)}`, typeError);
-			else
-				showAlert(`${status} ${error.responseText}`, typeError)
-		} else
-			showAlert("No hay conexión con el servidor.", typeError);
+        if (error.readyState !== 0) {
+            let status = `Error ${error.status}:`;
+            if (error.responseJSON !== undefined)
+                showAlert(`${status} ${JSON.stringify(error.responseJSON.errors)}`, typeError);
+            else
+                showAlert(`${status} ${error.responseText}<br>${secundaryMsg}`, typeError)
+        } else
+            showAlert("No hay conexión con el servidor.", typeError);
 
-	};
+    };
 
 
-	// Tests
-	var personas = [
-		{ name: "Pedro", surName: "Picapiedra", dni: "11223344" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "34234233" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "45445422" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "45422323" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "55522234" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "88852244" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "34342222" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "22463484" },
-		{ name: "Pedro", surName: "Picapiedra", dni: "45362342" }
-	]
-	//tableHtml.append(generateTableOf(personas));
+    // Tests
+    var personas = [
+        { name: "Pedro", surName: "Picapiedra", dni: "11223344" },
+        { name: "Pedro", surName: "Picapiedra", dni: "34234233" },
+        { name: "Pedro", surName: "Picapiedra", dni: "45445422" },
+        { name: "Pedro", surName: "Picapiedra", dni: "45422323" },
+        { name: "Pedro", surName: "Picapiedra", dni: "55522234" },
+        { name: "Pedro", surName: "Picapiedra", dni: "88852244" },
+        { name: "Pedro", surName: "Picapiedra", dni: "34342222" },
+        { name: "Pedro", surName: "Picapiedra", dni: "22463484" },
+        { name: "Pedro", surName: "Picapiedra", dni: "45362342" }
+    ]
+    //tableHtml.append(generateTableOf(personas));
 })
